69ee18d61bf7410c245bd0a46002c18fdb79c3b0
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-01-26 19:04:51 +0100

    styled ImgSlider

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,15 +18,14 @@ display: block;
 top: 72px;
 padding: 0 calc(3.5vw + 5px);
 &:after {
-    background: url("/images/home-background.png") center center / cover no-repeat fixed;
-    content: "";
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-
+  background: url("/images/home-background.png") center center / cover
+    no-repeat fixed;
+  content: "";
+  position: absolute;
+  inset: 0px;
+  opacity: 1;
+  z-index: -1;
 }
-
-`
+`;
 
 export default Home;
